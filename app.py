4c4689bb4b741a4b7db949b301a9fcd8a4571d4e
Basil George <87349509+BasilGeorge7@users.noreply.github.com>
2026-08-29 10:21:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Docker Deployment Successful V-1"
+    return "CI/CD Docker Deployment Successful V-3"
 
 app.run(host='0.0.0.0', port=5000)
